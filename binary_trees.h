22dfcdb5b89a8c32249b4d144a3b5ceb3040632a
Kenan <yahyayevkenan57@gmail.com>
2025-08-06 22:42:03 +0400

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,30 @@
-typedef struct binary_tree_s
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/* Prototype for task 0 */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* other prototypes (if any) */
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* BINARY_TREES_H */
