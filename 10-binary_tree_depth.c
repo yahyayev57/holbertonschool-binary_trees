eb34ffdaa096b65be5ac7799bddd114075e85ae9
Kenan <yahyayevkenan57@gmail.com>
2025-08-06 23:10:00 +0400

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,16 +8,16 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    size_t depth = 0;
+	size_t depth = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    while (tree->parent != NULL)
-    {
-        depth++;
-        tree = tree->parent;
-    }
+	while (tree->parent != NULL)
+	{
+		depth++;
+		tree = tree->parent;
+	}
 
-    return (depth);
+	return (depth);
 }
