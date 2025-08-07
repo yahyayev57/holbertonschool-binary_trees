cb349edeb37eb21ce805b35aa4b6a7140119c70c
Kenan <yahyayevkenan57@gmail.com>
2025-08-07 14:16:36 +0400

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 /* You may also have these optional helper functions */
 int binary_tree_height(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
-
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 #endif /* BINARY_TREES_H */
